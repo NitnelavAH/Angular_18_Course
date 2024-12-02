2c3c77cf43707c26dd65afc3d792d27dbd77948b
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2024-12-02 10:53:01 -0600

    finish course section of new features

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { booleanAttribute, Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './title.component.html',
+  imports: [],
+  template: `
+    <h1 class="text-3xl mb-5">
+      {{title}} @if (withShadow) { {{withShadow}} }
+    </h1>
+  `,
   styles: ``
 })
 export class TitleComponent {
+  @Input({required: true}) title!: string;
+  @Input({transform: booleanAttribute}) withShadow: boolean = false;
 
 }
